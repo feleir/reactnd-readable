fe1ad65b50e9f1b238984bbdf030a6bc9240bb2c
Ivan Diaz <feleir@gmail.com>
2018-01-23 12:00:32 +0100

    Updated styling for not found component.

--- ui/src/components/NotFound.js
+++ ui/src/components/NotFound.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const NotFound = (props) =>
-  <div>
+  <div className="container">
     <h3>This is not the {props.type} you are looking for.</h3>
     <a href="/">Go to main page</a>
   </div>
